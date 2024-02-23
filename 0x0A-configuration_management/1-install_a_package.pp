992cd387992b09b9eed6f150ad8c68e9869de742
OjElsa <elsahojwang06@gmail.com>
2024-02-23 15:22:24 -0800

    Installing a package using puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+# Install a package Using Puppet, install flask from pip3.
+
+package { 'Flask':
+ensure   => '2.1.0',
+provider => 'pip3'
+}
+
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3'
+}
