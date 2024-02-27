0db0caa00b4d017dd8553cafdfe0431afdb8a8aa
OjElsa <elsahojwang06@gmail.com>
2024-02-27 15:56:41 -0800

    Install nginx

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+package { 'nginx':
+  ensure => installed,
+}
+
+# Ensure index file is present and contains 'Hello World!'
+file { 'Website index file':
+  ensure  => present,
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+# Ensure 404 file is present and contains 'Ceci n'est pas une page'
+file { 'Website 404 file':
+  ensure  => present,
+  path    => '/var/www/html/404.html',
+  content => "Ceci n'est pas une page",
+}
+
+# Ensure nginx is running
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx']
+}
