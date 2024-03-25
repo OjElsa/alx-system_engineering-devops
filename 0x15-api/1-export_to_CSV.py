260736140add1fcba64e5e7e3ff7e384a62c0a5b
OjElsa <elsahojwang06@gmail.com>
2024-03-25 10:29:38 -0700

    code refactor

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Extending your Python script to export data in the CSV format. """
 
+import argparse
 import csv
 import requests
 import sys
@@ -9,8 +10,8 @@ import sys
 def get_employee_todo_progress(employee_id):
     """ Fetches and prints the TODO list progress of a given employee. """
     base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = base_url + 'users/' + str(employee_id)
-    todo_url = base_url + 'todos?userId=' + str(employee_id)
+    user_url = f"{base_url}users/{employee_id}"
+    todo_url = f"{base_url}todos?userId={employee_id}"
 
     try:
         user_response = requests.get(user_url)
@@ -24,7 +25,7 @@ def get_employee_todo_progress(employee_id):
 
         csv_file = f"{employee_id}.csv"
 
-        with open(csv_file, mode='w', newline='') as file:
+        with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file, quoting=csv.QUOTE_ALL)
             writer.writerow(["USER_ID",
                             "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
@@ -45,9 +46,8 @@ def get_employee_todo_progress(employee_id):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Export data in CSV format.")
+    parser.add_argument("employee_id", type=int, help="Employee ID")
+    args = parser.parse_args()
 
-    employee_id = sys.argv[1]
-    get_employee_todo_progress(employee_id)
+    get_employee_todo_progress(args.employee_id)
