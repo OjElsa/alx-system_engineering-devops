56f981da81322f798e24a5e44f978c1fa189fa86
OjElsa <elsahojwang06@gmail.com>
2024-03-25 13:37:54 -0700

    code refactor x2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Extending your Python script to export data in the CSV format. """
+"""Extending your Python script to export data in the CSV format."""
 
-import argparse
+
 import csv
 import requests
 import sys
@@ -10,8 +10,8 @@ import sys
 def get_employee_todo_progress(employee_id):
     """ Fetches and prints the TODO list progress of a given employee. """
     base_url = 'https://jsonplaceholder.typicode.com/'
-    user_url = f"{base_url}users/{employee_id}"
-    todo_url = f"{base_url}todos?userId={employee_id}"
+    user_url = base_url + 'users/' + str(employee_id)
+    todo_url = base_url + 'todos?userId=' + str(employee_id)
 
     try:
         user_response = requests.get(user_url)
@@ -23,7 +23,7 @@ def get_employee_todo_progress(employee_id):
         user_id = user_data.get('id')
         employee_name = user_data.get('username')
 
-        csv_file = f"{user_id}.csv"
+        csv_file = '{}.csv'.format(user_id)
 
         with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file, quoting=csv.QUOTE_ALL)
@@ -36,7 +36,7 @@ def get_employee_todo_progress(employee_id):
                                 task.get('completed'), task.get('title')])
                 task_count += 1
 
-        print(f"CSV file '{csv_file}' has been created.")
+        print("CSV file '{}' has been created.".format(csv_file))
         status = 'OK' if task_count == len(todo_data) else 'Incorrect'
         print(f"Number of tasks in CSV: {status}")
 
@@ -46,8 +46,9 @@ def get_employee_todo_progress(employee_id):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Export data in CSV format.")
-    parser.add_argument("employee_id", type=int, help="Employee ID")
-    args = parser.parse_args()
+    if len(sys.argv) != 2:
+        print("Usage: python3 {} <employee_id>".format(sys.argv[0]))
+        sys.exit(1)
 
-    get_employee_todo_progress(args.employee_id)
+    employee_id = sys.argv[1]
+    get_employee_todo_progress(employee_id)
