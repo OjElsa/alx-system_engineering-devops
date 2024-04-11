d2861b05d11b3579a7c7eccb92160757dd473640
OjElsa <elsahojwang06@gmail.com>
2024-04-10 18:18:34 -0700

    fix 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fix an Apache error that returning a 500 error.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
