9e59831d2bf76d2b05926d185c93b145dae201e7
OjElsa <elsahojwang06@gmail.com>
2024-04-17 10:10:18 -0700

    Debbug

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,18 @@
+# Puppet script to inpcrease nginx
+
+$nginx_ulimit = 'ULIMIT="-n 65535"'
+
+package { 'nginx':
+}
+
+file { '/etc/default/nginx':
+  ensure  => present,
+  content => $nginx_ulimit,
+  notify  => Service['nginx']
+}
+
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+}
